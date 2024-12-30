061c34927684c8ec024b197b362297d309cc84c9
Rui Da Silva <ruipacheco@aol.com>
2024-12-30 20:52:08 +0000

    Initial Commit

---
+++ facetestTests/facetestTests.swift
@@ -0,0 +1,17 @@
+//
+//  facetestTests.swift
+//  facetestTests
+//
+//  Created by Rui Da Silva on 30/12/2024.
+//
+
+import Testing
+@testable import facetest
+
+struct facetestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
